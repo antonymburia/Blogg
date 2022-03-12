b70a551a0fa43d66e9faecfdc9c73ff52fc24124
antonymburia <antonymburia10@gmail.com>
2022-03-12 22:01:57 +0300

    create user auth Views

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,19 +1,45 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField,SubmitField
-from wtforms.validators import DataRequired,Email,EqualTo
+from flask import render_template, redirect, url_for, flash
+from . import auth
+from flask_login import login_user, logout_user, login_required
+from ..models import User
+from .. import db,photos
+from .forms import LoginForm, RegistrationForm
+from ..email import mail_message
 
 
-class RegistrationForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
-    username = StringField('Enter your username',validators = [DataRequired()])
-    firstname = StringField('Enter your first name',validators = [DataRequired()])
-    lastname = StringField('Enter your last name',validators = [DataRequired()])
-    password = PasswordField('Password',validators = [DataRequired(), EqualTo('password_confirm',message = 'Passwords must match')])
-    password_confirm = PasswordField('Confirm Passwords',validators = [DataRequired()])
-    submit = SubmitField('Sign Up')
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(url_for('main.index'))
 
-class LoginForm(FlaskForm):
-    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
-    password = PasswordField('Password',validators =[DataRequired()])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Login')
+        flash('User not found')
+
+    
+    return render_template('auth/login.html',login_form = login_form)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,firstname= form.firstname.data,lastname= form.lastname.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        mail_message("Welcome to Pitch App","email/welcome_user", user.email,user=user)
+
+
+      
+
+        return redirect(url_for('auth.login'))
+        
+    return render_template('auth/register.html',registration_form = form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
