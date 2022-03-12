008c398096b3685f29dd9ad873154f85d53e7651
antonymburia <antonymburia10@gmail.com>
2022-03-12 21:56:54 +0300

    create and import app configurations

---
+++ config.py
@@ -0,0 +1,50 @@
+import os
+import re
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY = 'thisissecretkey'
+    
+
+    #email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+        
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+   
+    
+    
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class 
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/blogg'
+  
+
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
