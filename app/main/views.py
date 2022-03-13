d75307f633a37ca0701276ac228133bf103c40fd
antonymburia <antonymburia10@gmail.com>
2022-03-13 03:11:36 +0300

    create user profile template and its view

--- app/main/views.py
+++ app/main/views.py
@@ -15,3 +15,17 @@ def index():
 
     return render_template('index.html',name = name)
     
+
+@main.route('/user/<usersname>')
+def profile(usersname):
+    user = User.query.filter_by(username = usersname).first()
+    user_joined = user.date_joined.strftime('%b %d, %Y')
+
+
+    if user is None:
+        abort(404)
+        
+
+    
+
+    return render_template("profile/profile.html", user = user, date = user_joined)
